c2ba406308c845b0156701647a93e8f055d86458
Falk Schoenfeld <schoenfeld@magazino.eu>
2022-09-03 18:32:15 +0200

    fixed connectivity issues

--- src/footprints/button.js
+++ src/footprints/button.js
@@ -6,7 +6,8 @@ module.exports = {
     params: {
         class: 'B', // for Button
         side: 'F',
-        reverse: true
+        reverse: true,
+        outline: false
     },
     body: p => {
       const standard = `
@@ -17,44 +18,56 @@ module.exports = {
 
             ${p.at /* parametric position */}
       `
+      function outline(side){
+        if(p.param.outline){
+          return `
+              ${'' /* outline */}
+              (fp_line (start 2.75 1.25) (end 1.25 2.75) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start 2.75 -1.25) (end 1.25 -2.75) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start 2.75 -1.25) (end 2.75 1.25) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start -1.25 2.75) (end 1.25 2.75) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start -1.25 -2.75) (end 1.25 -2.75) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start -2.75 1.25) (end -1.25 2.75) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start -2.75 -1.25) (end -1.25 -2.75) (layer ${side}.SilkS) (width 0.15))
+              (fp_line (start -2.75 -1.25) (end -2.75 1.25) (layer ${side}.SilkS) (width 0.15))
+          `
+        }else{
+          return ``
+        }
+      }
       function footprint(side) {
         return `
             ${'' /* footprint reference */}
             (fp_text reference "${p.ref}" (at 0 0) (layer ${side}.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
             (fp_text value "" (at 0 0) (layer ${side}.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
-            
-            ${'' /* outline */}
-            (fp_line (start 2.75 1.25) (end 1.25 2.75) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start 2.75 -1.25) (end 1.25 -2.75) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start 2.75 -1.25) (end 2.75 1.25) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start -1.25 2.75) (end 1.25 2.75) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start -1.25 -2.75) (end 1.25 -2.75) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start -2.75 1.25) (end -1.25 2.75) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start -2.75 -1.25) (end -1.25 -2.75) (layer ${side}.SilkS) (width 0.15))
-            (fp_line (start -2.75 -1.25) (end -2.75 1.25) (layer ${side}.SilkS) (width 0.15))
-            
             ${'' /* pins */}
-            (pad 1 smd rect (at -3.1 -1.85 ${p.rot}) (size 1.8 1.1) (layers ${side}.Cu ${side}.Paste ${side}.Mask))
+            (pad 1 smd rect (at -3.1 -1.85 ${p.rot}) (size 1.8 1.1) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.from.str})
             (pad 1 smd rect (at 3.1 -1.85 ${p.rot}) (size 1.8 1.1) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.from.str})
             (pad 2 smd rect (at -3.1 1.85 ${p.rot}) (size 1.8 1.1) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.to.str})
-            (pad 2 smd rect (at 3.1 1.85 ${p.rot}) (size 1.8 1.1) (layers ${side}.Cu ${side}.Paste ${side}.Mask))
+            (pad 2 smd rect (at 3.1 1.85 ${p.rot}) (size 1.8 1.1) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.to.str})
 
             ${'' /* connections */}
-            (fp_line (start -3.1 -1.85) (end 3.1 -1.85) (layer ${side}.Cu) (width 0.25))
-            (fp_line (start -3.1 1.85) (end 3.1 1.85) (layer ${side}.Cu) (width 0.25))
+            (pad 1 smd custom (at 0 -1.85 ${p.rot}) 
+            (size 0 0) (layers ${side}.Cu) ${p.net.from.str} 
+            (primitives (gr_line (start -3.1 0) (end 3.1 0) (width 0.25))))
+            (pad 2 smd custom (at 0 1.85 ${p.rot}) 
+            (size 0 0) (layers ${side}.Cu) ${p.net.to.str} 
+            (primitives (gr_line (start -3.1 0) (end 3.1 0) (width 0.25))))
         `
       }
       const vias = `
             ${''/* interconnect */}
-            (pad 1 thru_hole circle (at 0 -1.85) (size 0.6 0.6) (drill 0.2) (layers *.Cu *.Mask))
-            (pad 2 thru_hole circle (at 0  1.85) (size 0.6 0.6) (drill 0.2) (layers *.Cu *.Mask))
+            (pad 1 thru_hole circle (at 0 -1.85) (size 0.6 0.6) (drill 0.3) (layers *.Cu *.Mask) ${p.net.from.str})
+            (pad 2 thru_hole circle (at 0  1.85) (size 0.6 0.6) (drill 0.3) (layers *.Cu *.Mask) ${p.net.to.str})
       `
       
       if(p.param.reverse) {
         return `
           ${standard}
           ${footprint('F')}
+          ${outline('F')}
           ${footprint('B')}
+          ${outline('B')}
           ${vias}
           )
           `
@@ -62,6 +75,7 @@ module.exports = {
         return `
           ${standard}
           ${footprint(p.param.side)}
+          ${outline(p.param.side)}
           )
           `
       }
--- src/footprints/padpair.js
+++ src/footprints/padpair.js
@@ -12,7 +12,7 @@ module.exports = {
         distance: 1.2,
         side: 'F',
         reverse: true,
-        text: '',
+        text: undefined 
     },
     body: p => {
 
@@ -20,28 +20,34 @@ module.exports = {
             (module SMDPad (layer F.Cu) (tedit 5B24D78E)
             ${p.at /* parametric position */}
         `
+        const text = p.param.text ? `(fp_text user ${p.param.text} (at 0 ${p.param.height} ${p.rot}) (layer ${side}.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ${side == 'B' ? '(justify mirror)':''}))`:``
+
         function footprint(side) {
           return `
             ${'' /* footprint reference */}
-            (pad 1 smd rect (at ${(p.param.distance+p.param.width)/2} 0 ${p.rot}) (size ${p.param.width} ${p.param.height}) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.A.str} (zone_connect 0))
-            (pad 2 smd rect (at ${-(p.param.distance+p.param.width)/2} 0 ${p.rot}) (size ${p.param.width} ${p.param.height}) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.B.str} (zone_connect 0))
-            (fp_text user ${p.param.text} (at 0 ${p.param.height} ${p.rot}) (layer ${side}.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+            (pad 1 smd rect (at ${(p.param.distance+p.param.width)/2} 0 ${p.rot}) (size ${p.param.width} ${p.param.height}) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.A.str})
+            (pad 2 smd rect (at ${-(p.param.distance+p.param.width)/2} 0 ${p.rot}) (size ${p.param.width} ${p.param.height}) (layers ${side}.Cu ${side}.Paste ${side}.Mask) ${p.net.B.str})
+            ${text}
           `
         }
 
         const vias = `
           ${''/* THT terminals */}
-          (pad 1 thru_hole circle (at ${(p.param.distance+p.param.width)} 0 ${p.rot}) (size 0.6 0.6) (drill 0.2) (layers *.Cu *.Mask) (zone_connect 0))
-          (pad 2 thru_hole circle (at ${-(p.param.distance+p.param.width)} 0 ${p.rot}) (size 0.6 0.6) (drill 0.2) (layers *.Cu *.Mask) (zone_connect 0))
+          (pad 1 thru_hole circle (at ${(p.param.distance+p.param.width)} 0 ${p.rot}) (size 0.6 0.6) (drill 0.3) (layers *.Cu *.Mask) ${p.net.A.str})
+          (pad 2 thru_hole circle (at ${-(p.param.distance+p.param.width)} 0 ${p.rot}) (size 0.6 0.6) (drill 0.3) (layers *.Cu *.Mask) ${p.net.B.str})
 
           ${''/* connect THT and pads */}
-          (pad "" smd custom (at ${p.param.distance+p.param.width} 0 ${p.rot}) (size 0 0) (layers F.Cu) ${p.net.A.str} (zone_connect 0)
+          (pad "" smd custom (at ${p.param.distance+p.param.width} 0 ${p.rot}) 
+            (size 0 0) (layers F.Cu) ${p.net.A.str} 
             (primitives (gr_line (start 0 0) (end -${p.param.width} 0) (width 0.25))))
-          (pad "" smd custom (at ${-(p.param.distance+p.param.width)} 0 ${p.rot}) (size 0 0) (layers F.Cu) ${p.net.B.str} (zone_connect 0)
+          (pad "" smd custom (at ${-(p.param.distance+p.param.width)} 0 ${p.rot})
+            (size 0 0) (layers F.Cu) ${p.net.B.str} 
             (primitives (gr_line (start 0 0) (end ${p.param.width} 0) (width 0.25))))
-          (pad "" smd custom (at ${p.param.distance+p.param.width} 0 ${p.rot}) (size 0 0) (layers B.Cu) ${p.net.A.str} (zone_connect 0)
+          (pad "" smd custom (at ${p.param.distance+p.param.width} 0 ${p.rot})
+            (size 0 0) (layers B.Cu) ${p.net.A.str} 
             (primitives (gr_line (start 0 0) (end -${p.param.width} 0) (width 0.25))))
-          (pad "" smd custom (at ${-(p.param.distance+p.param.width)} 0 ${p.rot}) (size 0 0) (layers B.Cu) ${p.net.B.str} (zone_connect 0)
+          (pad "" smd custom (at ${-(p.param.distance+p.param.width)} 0 ${p.rot})
+            (size 0 0) (layers B.Cu) ${p.net.B.str} 
             (primitives (gr_line (start 0 0) (end ${p.param.width} 0) (width 0.25))))
         `
         if(p.param.reverse){
